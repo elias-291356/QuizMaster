036a4bb3fa0aeedb89de9c42fa5071c81e1c60bc
elias-291356 <ikucher.002@gmail.com>
2024-02-08 11:18:11 +0400

    init project, basic redux setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+
+// import "./App.css";
+// import { useDispatch, useSelector } from "react-redux";
+// import { selectCount, selectIsSubmit } from "./redux/selector";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux/store";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        {/* <BrowserRouter basename="/QuizMaster"> */}
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
