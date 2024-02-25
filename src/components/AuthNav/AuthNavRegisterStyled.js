9cecb264f505ee47944318579311af33e07618ca
elias-291356 <ikucher.002@gmail.com>
2024-02-25 10:37:49 +0400

    styled login, layout router

---
+++ src/components/AuthNav/AuthNavRegisterStyled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+export const StyledAuthNavLinkRegisterWrap = styled(Link)`
+  color: var(--white);
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 1.14286;
+  letter-spacing: -0.01em;
+  text-decoration: underline;
+  text-decoration-skip-ink: none;
+`;
